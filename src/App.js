9741a870523858fe3958716d3e346c8b3a654ead
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-10 23:06:59 +0530

    added text-gray-700 class to app component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import About from './routes/About';
 function App() {
   return (
     <BrowserRouter>
-      <div className='antialiased'>
+      <div className='antialiased text-gray-700'>
         <Nav />
-        <Switch className='z-20'>
+        <Switch className=''>
           <Route path='/shop' exact component={Shop}></Route>
           <Route path='/about' exact component={About}></Route>
           <Route path='/shop/:id' exact component={ItemDetail}></Route>

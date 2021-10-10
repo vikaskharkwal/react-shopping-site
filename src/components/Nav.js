f2a2c61823a0f9f99fde86f1a75eebf7ddb17c03
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-10 23:08:29 +0530

    added relevant fontawesome icons to nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,9 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoePrints } from '@fortawesome/free-solid-svg-icons';
+import { faStore } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { NavLink, Link } from 'react-router-dom';
 import MenuButton from './MenuButton';
 function Nav(props) {
@@ -10,13 +15,14 @@ function Nav(props) {
 
   return (
     <header
-      className={`flex justify-center bg-white md:shadow-md ${
+      className={`flex justify-center bg-white md:shadow-md sticky top-0 ${
         !isNavOpen ? 'shadow-md' : ''
       }`}>
-      <div className='uppercase w-full 2xl:container flex justify-between align-middle md:px-8  relative'>
+      <div className='w-full 2xl:container flex justify-between align-middle md:px-8  relative'>
         <Link to='/'>
           <h2 className='text-green-700 text-xl font-bold p-4 hover:bg-green-50 transition duration-500'>
-            Company
+            <FontAwesomeIcon icon={faShoePrints} />
+            <span className='inline-block ml-2'>eKICKZ</span>
           </h2>
         </Link>
         <MenuButton handleToggle={handleToggle} isNavOpen={isNavOpen} />
@@ -24,33 +30,36 @@ function Nav(props) {
         <nav
           className={`${
             !isNavOpen ? 'scale-y-0' : 'scale-y-100'
-          } transform flex sm:inline-flex flex-col absolute w-full left-0 top-full sm:w-max sm:flex-row sm:static sm:align-middle sm:justify-center shadow-md sm:shadow-none bg-white transition duration-500 origin-top delay sm:scale-y-100`}>
+          } uppercase transform flex sm:inline-flex flex-col absolute w-full left-0 top-full sm:w-max sm:flex-row sm:static sm:align-middle sm:justify-center shadow-md sm:shadow-none bg-white transition duration-500 origin-top delay sm:scale-y-100`}>
           <NavLink
             exact
             activeClassName='border-green-700 bg-green-50 ease-in font-semibold text-green-700'
-            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100 ${
+            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100  ${
               isNavOpen ? 'opacity-100' : 'opacity-0'
             }`}
             to='/'>
-            Home
+            <FontAwesomeIcon icon={faHome} />
+            <span className='inline-block ml-2'>Home</span>
           </NavLink>
           <NavLink
             exact
             activeClassName='border-b-4 border-green-700 bg-green-50 font-semibold text-green-700'
-            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100 ${
+            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100  ${
               isNavOpen ? 'opacity-100' : 'opacity-0'
             }`}
             to='/shop'>
-            Shop
+            <FontAwesomeIcon icon={faStore} />
+            <span className='inline-block ml-2'>Store</span>
           </NavLink>
           <NavLink
             exact
             activeClassName='border-b-4 border-green-700 bg-green-50 font-semibold text-green-700'
-            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100 ${
+            className={`hover:bg-green-50 p-4 text-right border-b-4 border-transparent transition duration-200 sm:opacity-100  ${
               isNavOpen ? 'opacity-100' : 'opacity-0'
             }`}
             to='/about'>
-            About
+            <FontAwesomeIcon icon={faInfoCircle} />
+            <span className='inline-block ml-2'>About</span>
           </NavLink>
         </nav>
       </div>

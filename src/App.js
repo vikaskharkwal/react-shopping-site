0c50d061b00b9486d6a06d6fe49be328102454f1
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-12 09:07:01 +0530

    modified to pass props to the routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import Shop from './routes/Shop';
 import ItemDetail from './routes/ItemDetail';
 import Nav from './components/Nav';
 import About from './routes/About';
+import { useState } from 'react';
 function App() {
+  const [theme] = useState('blue');
   return (
     <BrowserRouter>
       <div className='antialiased text-gray-700'>
-        <Nav />
-        <Switch className=''>
-          <Route path='/shop' exact component={Shop}></Route>
-          <Route path='/about' exact component={About}></Route>
-          <Route path='/shop/:id' exact component={ItemDetail}></Route>
-          <Route path='/' exact component={Home}></Route>
-        </Switch>
+        <Nav theme={theme} />
+        <div className='ml-auto mr-auto relative z-10'>
+          <Switch>
+            <Route
+              path='/shop'
+              exact
+              render={(props) => <Shop theme={theme} />}></Route>
+            <Route
+              path='/about'
+              exact
+              render={(props) => <About theme={theme} />}></Route>
+            <Route
+              path='/shop/:id'
+              exact
+              render={(props) => <ItemDetail theme={theme} />}></Route>
+            <Route
+              path='/'
+              exact
+              render={(props) => <Home theme={theme} />}></Route>
+          </Switch>
+        </div>
       </div>
     </BrowserRouter>
   );

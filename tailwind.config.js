631ec7936a65f9078a7ac4ce837ffc67e0cda881
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-14 17:00:13 +0530

    added custom utilities and extended variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+var flattenColorPalette =
+  require('tailwindcss/lib/util/flattenColorPalette').default;
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -5,7 +8,24 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      scale: 'group-hover',
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities, e, theme, variants }) => {
+      const colors = flattenColorPalette(theme('borderColor'));
+      delete colors['default'];
+
+      const colorMap = Object.keys(colors).map((color) => ({
+        [`.border-t-${color}`]: { borderTopColor: colors[color] },
+        [`.border-r-${color}`]: { borderRightColor: colors[color] },
+        [`.border-b-${color}`]: { borderBottomColor: colors[color] },
+        [`.border-l-${color}`]: { borderLeftColor: colors[color] },
+      }));
+      const utilities = Object.assign({}, ...colorMap);
+
+      addUtilities(utilities, variants('borderColor'));
+    },
+  ],
 };

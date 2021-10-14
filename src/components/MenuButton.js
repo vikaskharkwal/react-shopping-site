87d0009a3c797cf78f6774a91ebb01def9193198
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-14 19:00:29 +0530

    made design changes

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -2,11 +2,14 @@ function MenuButton({ handleToggle, isNavOpen, theme }) {
   return (
     <div
       onClick={handleToggle}
-      className={`relative sm:hidden flex w-14 h-full flex-col justify-between px-4 py-5 hover:bg-${theme}-50 cursor-pointer `}>
+      className={`relative sm:hidden flex  h-full flex-col justify-between px-4 py-5 cursor-pointer transition duration-300 ${
+        isNavOpen ? 'hover:bg-red-50' : `hover:bg-${theme}-50`
+      } `}
+      style={{ width: '3.9rem' }}>
       <div
-        className={`origin-left w-full h-0.5  transition duration-500 ${
+        className={`origin-left w-full h-0.5 transition duration-500 ${
           isNavOpen
-            ? 'transform rotate-45 bg-red-500 top-px relative'
+            ? 'transform rotate-45 bg-red-500 relative'
             : `bg-${theme}-700`
         }`}></div>
       <div
@@ -14,7 +17,7 @@ function MenuButton({ handleToggle, isNavOpen, theme }) {
           isNavOpen ? 'opacity-0' : `bg-${theme}-700`
         }`}></div>
       <div
-        className={`origin-left w-full h-0.5  transition duration-500 ${
+        className={`origin-left w-full h-0.5 mb-px transition duration-500 ${
           isNavOpen ? 'transform -rotate-45 bg-red-500' : `bg-${theme}-700`
         }`}></div>
     </div>

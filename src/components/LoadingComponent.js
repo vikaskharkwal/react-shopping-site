2a5c40ce47a50886f5a4abde9af749c1b0c6a9cf
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-14 19:00:11 +0530

    designed the component to show when loading data

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from 'react';
+
+function LoadingComponent({ theme, text }) {
+  const [isError, setIsError] = useState(false);
+  const errorRef = useRef(null);
+
+  const clear = () => {
+    if (errorRef.current) {
+      clearTimeout(errorRef.current);
+    }
+  };
+
+  useEffect(() => {
+    errorRef.current = setTimeout(() => {
+      setIsError(true);
+    }, 10000);
+    return () => {
+      clear();
+    };
+  }, []);
+
+  return !isError ? (
+    <div
+      className={`flex items-center flex-col sm:flex-row justify-center gap-4 col-span-full h-full row-span-full `}>
+      <div
+        className={`h-12 w-12 bg-transparent border-4 border-gray-300 border-r-${theme}-700  animate-spin rounded-full`}></div>
+      <p className={`text-2xl text-gray-400`}>Loading {text}</p>
+    </div>
+  ) : (
+    <p
+      className={`text-xl font-thin col-span-full text-center mt-4 text-gray-400 `}>
+      Error Loading {text}
+    </p>
+  );
+}
+
+export default LoadingComponent;

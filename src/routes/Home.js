26763d82ca146bfa4a4da1bb10f41d1ca3ef8529
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-14 19:26:24 +0530

    designed the homepage and added the image carousel

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,23 @@
 import ImageCarousel from '../components/ImageCarousel';
 import { Link } from 'react-router-dom';
 
-function Home(props) {
+function Home({ theme }) {
+  document.title = 'KICKZ - One Stop Shop For All Your Sneaker Needs';
+
   return (
-    <div className='relative'>
-      <ImageCarousel delay='5000' />
+    <div className='relative z-0'>
+      <ImageCarousel delay='3000' />
       <div className='absolute inset-0 bg-gradient-to-tr from-black to-transparent z-10'>
-        <div className='absolute inset-0 px-4 md:px-16 py-12 text-white flex flex-col gap-1 md:gap-2 justify-end'>
-          <h1 className='col-span-3 text-left text-3xl sm:text-5xl lg:6xl font-bold uppercase md:text-justify relative'>
-            Hundreds of Sneakers to choose from
+        <div className='absolute inset-0 px-4 md:px-16 py-12 text-white flex flex-col md:gap-2 justify-end'>
+          <h1 className='col-span-3 text-left text-3xl sm:text-5xl lg:6xl font-bold uppercase relative'>
+            One Stop Shop For All Your Sneaker Needs
           </h1>
-          <p className='text-xl md:text-3xl w-max font-light'>
-            make 'em yours...
+          <p className='text-lg md:text-2xl w-max font-light uppercase'>
+            Thousands of Sneakers to choose from
           </p>
           <Link
             to='/shop'
-            className={`w-max uppercase font-medium px-2 py-1 text-xl md:text-2xl md:px-4 md:py-3 bg-${props.theme}-700 text-white transition duration-100 transform hover:scale-105 hover:translate-x-1 focus:scale-95 focus:translate-x-0`}>
+            className={`mt-4 w-max uppercase font-medium px-2 py-1 text-xl md:text-2xl md:px-4 md:py-3 bg-${theme}-700 text-white transition duration-100 transform hover:bg-${theme}-600`}>
             Visit Shop
           </Link>
         </div>
